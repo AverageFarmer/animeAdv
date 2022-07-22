d24171b462b5b1cc82dc0578acb64d786195af66
Dizziee <66963438+Dizziee@users.noreply.github.com>
2022-07-22 01:30:16 -0400

    asd

--- AutoFarm.lua
+++ AutoFarm.lua
@@ -67,6 +67,13 @@ local Settings = {
     },
 }
 
+local vu = game:GetService("VirtualUser")
+
+game:GetService("Players").LocalPlayer.Idled:Connect(function()
+	vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+	task.wait(.1)
+	vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
 
 for i,v in pairs(HttpService:JSONDecode(SavedSettings)) do
     Settings[i] = v
